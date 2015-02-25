bad12217b7f9fbad69378df058d85879ccc711a5
ren <ren_jiaxing@163.com>
2015-02-25 11:25:56 +0800

    Initial Commit

---
+++ gogu03/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gogu03
+//
+//  Created by ren on 15/2/25.
+//  Copyright (c) 2015年 ren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gogu03/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gogu03
+//
+//  Created by ren on 15/2/25.
+//  Copyright (c) 2015年 ren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
